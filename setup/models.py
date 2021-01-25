8aa5c279f86c6d2ce716bb60e2479f0f7bd56ba4
IvanTepes <ivan.tepes@outlook.com>
2021-01-25 23:41:51 +0000

    Database update

--- setup/models.py
+++ setup/models.py
@@ -1,16 +1,13 @@
 from django.db import models
 
 
-
 # Create your models here.
 
 
 class Brand(models.Model):
 
     name = models.CharField(max_length=254)
-    friendly_name = models.CharField(max_length=254, null=True, blank=False)
-    has_logo = models.BooleanField(
-            default=True, null=True, blank=False)
+    friendly_name = models.CharField(null=True, max_length=254, blank=False)
     brand_logo = models.ImageField(null=True, blank=True)
     has_side_banner = models.BooleanField(default=False, null=True, blank=True)
     side_banner = models.ImageField(null=True, blank=True)
@@ -51,36 +48,41 @@ class KeyFeatures(models.Model):
       default='ProductCategory Features', max_length=254)
 
     feature_for = models.CharField(
-      max_length=254, null=True, blank=True,
+      max_length=254, blank=True,
       default='Motherboard, Graphic Card etc. ')
 
     feature_1_name = models.ForeignKey(
       'Feature', related_name='feature_1_names', null=True,
       blank=True, on_delete=models.SET_NULL)
+
     feature_1 = models.CharField(
       max_length=254, blank=True)
 
     feature_2_name = models.ForeignKey(
       'Feature', related_name='feature_2_names', null=True,
       blank=True, on_delete=models.SET_NULL)
+
     feature_2 = models.CharField(
       max_length=254, blank=True)
 
     feature_3_name = models.ForeignKey(
       'Feature', related_name='feature_3_names', null=True,
       blank=True, on_delete=models.SET_NULL)
+
     feature_3 = models.CharField(
       max_length=254, blank=True)
 
     feature_4_name = models.ForeignKey(
       'Feature', related_name='feature_4_names', null=True,
       blank=True, on_delete=models.SET_NULL)
+
     feature_4 = models.CharField(
       max_length=254, blank=True)
 
     feature_5_name = models.ForeignKey(
       'Feature', default='test1', related_name='feature_5_names',
       null=True, blank=True, on_delete=models.SET_NULL)
+
     feature_5 = models.CharField(
       max_length=254, blank=True)
 
@@ -107,53 +109,102 @@ class Specification(models.Model):
     class Meta:
         verbose_name_plural = 'Specification'
 
-    name = models.CharField(default='of Specification!', max_length=254)
+    name = models.CharField(
+      default='of Specification!', max_length=254)
 
-    specification_for = models.CharField(max_length=254, null=True, blank=False, default='Motherboard, Graphic Card, etc. ')
+    specification_for = models.CharField(
+      max_length=254, null=True, blank=False,
+      default='Motherboard, Graphic Card, etc. ')
 
-    spec_1_name = models.ForeignKey('Spec', related_name='spec_1_names', null=True, blank=False, on_delete=models.SET_NULL)
-    spec_1 = models.CharField(max_length=254, blank=False, default="Unfortunately we do not have any specification for this product. ")
+    spec_1_name = models.ForeignKey(
+      'Spec', related_name='spec_1_names', null=True, blank=False,
+      on_delete=models.SET_NULL)
 
-    spec_2_name = models.ForeignKey('Spec', related_name='spec_2_names', null=True, blank=True, on_delete=models.SET_NULL)
+    spec_1 = models.CharField(
+      max_length=254, blank=False,
+      default="Unfortunately we do not have any specification for this product. ")
+
+    spec_2_name = models.ForeignKey(
+      'Spec', related_name='spec_2_names', null=True, blank=True,
+      on_delete=models.SET_NULL)
+
     spec_2 = models.CharField(max_length=254, blank=True)
 
-    spec_3_name = models.ForeignKey('Spec', related_name='spec_3_names', null=True, blank=True, on_delete=models.SET_NULL)
+    spec_3_name = models.ForeignKey(
+      'Spec', related_name='spec_3_names', null=True, blank=True,
+      on_delete=models.SET_NULL)
+
     spec_3 = models.CharField(max_length=254, blank=True)
 
-    spec_4_name = models.ForeignKey('Spec', related_name='spec_4_names', null=True, blank=True, on_delete=models.SET_NULL)
+    spec_4_name = models.ForeignKey(
+      'Spec', related_name='spec_4_names', null=True, blank=True,
+      on_delete=models.SET_NULL)
+
     spec_4 = models.CharField(max_length=254, blank=True)
 
-    spec_5_name = models.ForeignKey('Spec', related_name='spec_5_names', null=True, blank=True, on_delete=models.SET_NULL)
+    spec_5_name = models.ForeignKey(
+      'Spec', related_name='spec_5_names', null=True, blank=True,
+      on_delete=models.SET_NULL)
+
     spec_5 = models.CharField(max_length=254, blank=True)
 
-    spec_6_name = models.ForeignKey('Spec', related_name='spec_6_names', null=True, blank=True, on_delete=models.SET_NULL)
+    spec_6_name = models.ForeignKey(
+      'Spec', related_name='spec_6_names', null=True, blank=True,
+      on_delete=models.SET_NULL)
+
     spec_6 = models.CharField(max_length=254, blank=True)
 
-    spec_7_name = models.ForeignKey('Spec', related_name='spec_7_names', null=True, blank=True, on_delete=models.SET_NULL)
+    spec_7_name = models.ForeignKey(
+      'Spec', related_name='spec_7_names', null=True, blank=True,
+      on_delete=models.SET_NULL)
+
     spec_7 = models.CharField(max_length=254, blank=True)
 
-    spec_8_name = models.ForeignKey('Spec', related_name='spec_8_names', null=True, blank=True, on_delete=models.SET_NULL)
+    spec_8_name = models.ForeignKey(
+      'Spec', related_name='spec_8_names', null=True,
+      blank=True, on_delete=models.SET_NULL)
+
     spec_8 = models.CharField(max_length=254, blank=True)
 
-    spec_9_name = models.ForeignKey('Spec', related_name='spec_9_names', null=True, blank=True, on_delete=models.SET_NULL)
+    spec_9_name = models.ForeignKey(
+      'Spec', related_name='spec_9_names', null=True, blank=True,
+      on_delete=models.SET_NULL)
+
     spec_9 = models.CharField(max_length=254, blank=True)
 
-    spec_10_name = models.ForeignKey('Spec', related_name='spec_10_names', null=True, blank=True, on_delete=models.SET_NULL)
+    spec_10_name = models.ForeignKey(
+      'Spec', related_name='spec_10_names', null=True, blank=True,
+      on_delete=models.SET_NULL)
     spec_10 = models.CharField(max_length=254, blank=True)
 
-    spec_11_name = models.ForeignKey('Spec', related_name='spec_11_names', null=True, blank=True, on_delete=models.SET_NULL)
+    spec_11_name = models.ForeignKey(
+      'Spec', related_name='spec_11_names', null=True, blank=True,
+      on_delete=models.SET_NULL)
+
     spec_11 = models.CharField(max_length=254, blank=True)
 
-    spec_12_name = models.ForeignKey('Spec', related_name='spec_12_names', null=True, blank=True, on_delete=models.SET_NULL)
+    spec_12_name = models.ForeignKey(
+      'Spec', related_name='spec_12_names', null=True, blank=True,
+      on_delete=models.SET_NULL)
+
     spec_12 = models.CharField(max_length=254, blank=True)
 
-    spec_13_name = models.ForeignKey('Spec', related_name='spec_13_names', null=True, blank=True, on_delete=models.SET_NULL)
+    spec_13_name = models.ForeignKey(
+      'Spec', related_name='spec_13_names', null=True,
+      blank=True, on_delete=models.SET_NULL)
+
     spec_13 = models.TextField(blank=True)
 
-    spec_14_name = models.ForeignKey('Spec', related_name='spec_14_names', null=True, blank=True, on_delete=models.SET_NULL)
+    spec_14_name = models.ForeignKey(
+      'Spec', related_name='spec_14_names', null=True, blank=True,
+      on_delete=models.SET_NULL)
+
     spec_14 = models.TextField(blank=True)
 
-    spec_15_name = models.ForeignKey('Spec', related_name='spec_15_names', null=True, blank=True, on_delete=models.SET_NULL)
+    spec_15_name = models.ForeignKey(
+      'Spec', related_name='spec_15_names', null=True, blank=True,
+      on_delete=models.SET_NULL)
+
     spec_15 = models.TextField(blank=True)
 
     def __str__(self):
